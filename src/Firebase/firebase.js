dad9d03c0b267dd6b6615c433f26e2100d4479e1
Kamal <KamalJoshi2206@gamil.com>
2025-04-21 19:28:02 +0530

    updated

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyCTJpPGdZbz2InxtPWCUsh6P-X-KF3YGQk",
+  authDomain: "travelgameagency.firebaseapp.com",
+  projectId: "travelgameagency",
+  storageBucket: "travelgameagency.appspot.com",
+  messagingSenderId: "465824052309",
+  appId: "1:465824052309:web:fe05077ec5c695750e90dc"
 };
 
 const app = initializeApp(firebaseConfig);
